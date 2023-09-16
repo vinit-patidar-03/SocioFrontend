5846ddb5665333ecd3caaa1ec938013d863c40bd
Vinit Patidar <Vinitpatidar780@gmail.com>
2023-09-16 18:47:28 +0530

    "changes"

--- src/components/CreatedPostCard.js
+++ src/components/CreatedPostCard.js
@@ -35,13 +35,13 @@ const CreatedPostCard = (props) => {
 
   return (
     <>{user &&
-      <div className='p-3 bg-[#ffffff] mx-auto my-3 rounded-lg  w-[700px] createdPostCard'>
+      <div className='p-3 bg-[#ffffff] mx-auto my-3 rounded-lg  w-[50%] createdPostCard'>
         <div className='m-2 flex items-center'>
           <img src="https://cdn-icons-png.flaticon.com/128/3135/3135715.png" className='w-8' alt="logo" />
           <h5 className='ml-2'>{post.name}</h5>
         </div>
         <div className='m-auto p-2'>
-          <img src={post.photo} className='m-auto object-contain w-[675px]' alt="logo" />
+          <img src={post.photo} className='m-auto object-contain w-[90%]' alt="logo" />
         </div>
         <i className={`fa-${post.likes.filter((elem)=>{return elem === user._id}).length !== 0?"solid":"regular"} fa-heart ml-2 cursor-pointer fa-lg ${post.likes.filter((elem)=>{return elem === user._id}).length !== 0?"text-pink-500":""}`} onClick={likepostandUpdate} title='like/dislike'></i>
         <p className='ml-2'>{post.likes.length} likes</p>
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -2,29 +2,26 @@ import React, { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 
 const SignUp = () => {
-    
-    const Navigate = useNavigate();
-    const [credentials,setCredentials] = useState({name:'',email:'',password:'',confirm_password:''});
 
-    const userDetails = (event)=>
-    {
-        setCredentials({...credentials,[event.target.name]:event.target.value});
+    const Navigate = useNavigate();
+    const [credentials, setCredentials] = useState({ name: '', email: '', password: '', confirm_password: '' });
+
+    const userDetails = (event) => {
+        setCredentials({ ...credentials, [event.target.name]: event.target.value });
     }
 
-    const Submit = async (event)=>
-    {
+    const Submit = async (event) => {
         event.preventDefault();
-        const response = await fetch('https://socio-backend-seven.vercel.app/instagram/auth/signup',{
-            method : "POST",
-            headers : {
-                "Content-Type" : "application/json"
+        const response = await fetch('https://socio-backend-seven.vercel.app/instagram/auth/signup', {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
             },
-            body : JSON.stringify({name: credentials.name, email: credentials.email, password: credentials.password, confirm_password: credentials.confirm_password})
+            body: JSON.stringify({ name: credentials.name, email: credentials.email, password: credentials.password, confirm_password: credentials.confirm_password })
         });
         const data = await response.json();
-        if(data.success)
-        {
-            localStorage.setItem('token1',data.authToken);
+        if (data.success) {
+            localStorage.setItem('token1', data.authToken);
             Navigate('/');
         }
     }
@@ -38,20 +35,20 @@ const SignUp = () => {
                     <div className='p-3'>
                         <form action="login" className='flex flex-col items-center'>
                             <div className='userInput relative'>
-                                <input type="text" className='my-2 py-1 px-2 text-sm bg-slate-200 outline-none border-b-2 border-black' id='name' autoComplete='name' name='name' required onChange={userDetails}/>
+                                <input type="text" className='my-2 py-1 px-2 text-sm bg-slate-200 outline-none border-b-2 border-black' id='name' autoComplete='name' name='name' required onChange={userDetails} />
                                 <label htmlFor="name" className='absolute left-0 py-2 text-slate-500 cursor-text'>name</label>
                             </div>
                             <div className='userInput relative'>
-                                <input type="email" className='my-2 py-1 px-2 text-sm bg-slate-200 outline-none border-b-2 border-black' id='email' autoComplete='email' name='email' required onChange={userDetails}/>
+                                <input type="email" className='my-2 py-1 px-2 text-sm bg-slate-200 outline-none border-b-2 border-black' id='email' autoComplete='email' name='email' required onChange={userDetails} />
                                 <label htmlFor="email" className='absolute left-0 py-2 text-slate-500 cursor-text'>email</label>
                             </div>
                             <div className='passInput relative'>
-                                <input type="password" className='my-2 py-1 px-2 text-sm bg-slate-200 outline-none border-b-2 border-black' id='password' autoComplete='new-password' name='password' required onChange={userDetails}/>
+                                <input type="password" className='my-2 py-1 px-2 text-sm bg-slate-200 outline-none border-b-2 border-black' id='password' autoComplete='new-password' name='password' required onChange={userDetails} />
                                 <label htmlFor="create_password" className='absolute left-0 py-2 text-slate-500 cursor-text'>create password</label>
                             </div>
 
                             <div className='passInput relative'>
-                                <input type="password" className='my-2 py-1 px-2 text-sm bg-slate-200 outline-none border-b-2 border-black' id='confirm_password' autoComplete='new-password' name='confirm_password' required onChange={userDetails}/>
+                                <input type="password" className='my-2 py-1 px-2 text-sm bg-slate-200 outline-none border-b-2 border-black' id='confirm_password' autoComplete='new-password' name='confirm_password' required onChange={userDetails} />
                                 <label htmlFor="confirm_password" className='absolute left-0 py-2 text-slate-500 cursor-text'>confirm password</label>
                             </div>
 
