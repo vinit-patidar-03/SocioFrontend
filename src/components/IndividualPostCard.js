118a7ab27e8d75bae2a4176a48d29d4bdf26728c
Vinit Patidar <Vinitpatidar780@gmail.com>
2023-09-23 16:07:14 +0530

    changes

--- src/components/IndividualPostCard.js
+++ src/components/IndividualPostCard.js
@@ -3,33 +3,31 @@ import { useNavigate } from 'react-router-dom';
 
 const IndividualPostCard = (props) => {
   const Navigate = useNavigate('');
-    const {post} = props;
+  const { post } = props;
 
-    const deletePost = async ()=>
-    {
-        const response = await fetch('https://socio-backend-seven.vercel.app/instagram/posts/deletePost',{
-          method : 'DELETE',
-          headers : {
-          'Content-Type' : 'application/json'
-          },
-          body : JSON.stringify({id : post._id})
-        })
+  const deletePost = async () => {
+    const response = await fetch('https://socio-backend-seven.vercel.app/instagram/posts/deletePost', {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ id: post._id })
+    })
 
-        const result = await response.json();
+    const result = await response.json();
 
-        if(result.success === true)
-        {
-          Navigate('/');
-        }
+    if (result.success === true) {
+      Navigate('/');
     }
+  }
   return (
     <>
-        <div className='cursor-pointer mt-10 ml-8 shadow-lg'>
-             <div className='relative h-[300px] transition-all hover:scale-110'>
-                    <img src={post.photo} className='h-full rounded-xl shadow-lg' alt="logo" />
-                    <i className="fa-solid fa-trash absolute right-3 top-3 cursor-pointer text-red-600" onClick={deletePost}></i>
-             </div>
+      <div className='cursor-pointer mt-10 p-5 shadow-lg'>
+        <div className='relative h-[300px] transition-all hover:scale-110'>
+          <img src={post.photo} className='h-full rounded-xl shadow-lg' alt="logo" />
+          <i className="fa-solid fa-trash absolute right-3 top-3 cursor-pointer text-red-600" onClick={deletePost}></i>
         </div>
+      </div>
     </>
   )
 }
