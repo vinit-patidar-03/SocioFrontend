d9a40e412e6bb98147a89c2691abb9db25afb11d
Vinit Patidar <Vinitpatidar780@gmail.com>
2023-09-23 16:21:44 +0530

    Commit

--- src/components/IndividualPostCard.js
+++ src/components/IndividualPostCard.js
@@ -10,7 +10,7 @@ const IndividualPostCard = (props) => {
       method: 'DELETE',
       headers: {
         'Content-Type': 'application/json'
-      },
+      }, 
       body: JSON.stringify({ id: post._id })
     })
  
