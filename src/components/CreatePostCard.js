2232eb2db35c4d42feb8d7e80da9b7caf6ed5e99
Vinit Patidar <Vinitpatidar780@gmail.com>
2023-10-05 19:03:20 +0530

    "changes"

--- src/components/CreatePostCard.js
+++ src/components/CreatePostCard.js
@@ -70,7 +70,7 @@ const PostCard = () => {
                 loading ?
                     <img src="/images/spinner.gif" alt="loader" />
                     :
-                    <div className=' p-2 w-[90%] m-auto bg-white rounded-2xl createdPostCard flex flex-col justify-evenly'>
+                    <div className=' p-2 w-[90%] m-auto bg-[#ff5200] text-white rounded-2xl createPostCard flex flex-col justify-evenly'>
                         <h1 className='p-2 text-center font-bold text-xl'>Create Post and Share Your Best Time</h1>
                         <hr />
                         <div className='m-2 flex items-center'>
@@ -83,8 +83,8 @@ const PostCard = () => {
                         <div className='p-2'>
                             <input type="file" name="file" id="file" className='my-2 text-xs' onChange={selectFile} />
                             <div className='flex'>
-                                <textarea name="description" id="description" className='border-2 border-gray-400 text-black px-2 text-sm w-full' placeholder='write something about your post...' onChange={(event) => { setText(event.target.value) }}></textarea>
-                                <button className='py-1 ml-2 px-5 rounded-full bg-gray-300 hover:bg-gray-400 transition-all' onClick={savePost}><i className="fa-solid fa-arrow-right fa-xl"></i></button>
+                                <textarea name="description" id="description" className='border-2  text-black px-2 text-sm w-full' placeholder='write something about your post...' onChange={(event) => { setText(event.target.value) }}></textarea>
+                                <button className='py-1 ml-2 px-5 rounded-full bg-[#ff8f00] transition-all border-4 border-white hover:bg-orange-500 hover:transition-all' onClick={savePost}><i className="fa-solid fa-arrow-right fa-xl"></i></button>
                             </div>
                         </div>
                     </div>
