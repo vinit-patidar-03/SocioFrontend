cb34916d843e754bb78c81e42be23ce8bf363717
Vinit Patidar <Vinitpatidar780@gmail.com>
2023-10-08 16:29:48 +0530

    "changes"

--- src/context/AppContext.js
+++ src/context/AppContext.js
@@ -6,7 +6,7 @@ const AppContext = (props) => {
 
     const [user, setUser] = useState();
     const [data, setData] = useState();
-    const [message,setMessage] = useState('');
+    const [message, setMessage] = useState('');
 
     useEffect(() => {
         setTimeout(() => {
@@ -19,40 +19,42 @@ const AppContext = (props) => {
         if (status === "success") {
             toast.success(message);
         }
-        else if(status === "danger")
-        {
+        else if (status === "danger") {
             toast.error(message);
         }
     }
 
     const fetchUserDetails = async () => {
-        const response = await fetch('https://sociogrambackendapi.vercel.app/instagram/auth/getUser', {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json",
-                "authToken": localStorage.getItem('token1')
-            }
-        })
-
-        const userDetails = await response.json();
-        setUser(userDetails);
+        if (localStorage.getItem("token1") !== null) {
+            const response = await fetch('https://sociogrambackendapi.vercel.app/instagram/auth/getUser', {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json",
+                    "authToken": localStorage.getItem('token1')
+                }
+            })
+            const userDetails = await response.json();
+            setUser(userDetails);
+        }
     }
 
     const fetchPosts = async () => {
-        const response = await fetch('https://sociogrambackendapi.vercel.app/instagram/posts/posts',
-            {
-                method: "GET",
-                headers: {
-                    "Content-Type": "application/json"
-                }
-            });
+        if (localStorage.getItem('token1')) {
+            const response = await fetch('https://sociogrambackendapi.vercel.app/instagram/posts/posts',
+                {
+                    method: "GET",
+                    headers: {
+                        "Content-Type": "application/json"
+                    }
+                });
 
-        const result = await response.json();
-        setData(result.reverse());
+            const result = await response.json();
+            setData(result.reverse());
+        }
     }
 
     return (
-        <Context.Provider value={{ user, setUser, data, setData, fetchPosts, fetchUserDetails, showAlert,message,setMessage}}>
+        <Context.Provider value={{ user, setUser, data, setData, fetchPosts, fetchUserDetails, showAlert, message, setMessage }}>
             {props.children}
         </Context.Provider>
     )
