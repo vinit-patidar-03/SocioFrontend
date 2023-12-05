dd805bda34ac698592e262fe8eccd37839ed2634
Vinit Patidar <Vinitpatidar780@gmail.com>
2023-12-05 20:17:37 +0530

    "changes"

--- src/components/IndividualPostCard.js
+++ src/components/IndividualPostCard.js
@@ -13,7 +13,7 @@ const IndividualPostCard = (props) => {
     <>
       <div className='cursor-pointer mt-10 mx-2 p-5 w-[293px] flex justify-center bg-orange-500 rounded-lg'>
         <div className='relative h-[30vh] transition-all hover:scale-110'>
-          <img src={post.photo} className='h-full rounded-xl shadow-lg' alt="logo" />
+          <img src={post.photo.replace('http','https')} className='h-full rounded-xl shadow-lg' alt="logo" />
           <i className="fa-solid fa-trash absolute right-3 top-3 cursor-pointer text-red-600" onClick={deletePostAlert}></i>
         </div>
       </div>
--- src/pages/OtherUserProfileCard.js
+++ src/pages/OtherUserProfileCard.js
@@ -64,7 +64,7 @@ const OtherUserProfileCard = () => {
                             return (
                                 <div className='cursor-pointer mt-10 mx-2 p-5 w-[293px] flex justify-center bg-orange-500 rounded-lg' key={index}>
                                     <div className='relative h-[30vh] transition-all hover:scale-110'>
-                                        <img src={elem.photo} className='h-full rounded-xl shadow-lg' alt="logo" />
+                                        <img src={elem.photo.replace('http','https')} className='h-full rounded-xl shadow-lg' alt="logo" />
                                     </div>
                                 </div>
                             )
